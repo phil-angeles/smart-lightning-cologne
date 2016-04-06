09d145d2bcd4c7524a89cec13f890a7cb5cf2da2
Bastian Vrba <bvrba@posteo.de>
2016-04-06 10:49:34 +0200

    getKoordianten - für Laterne

--- smart-light/src/main/java/nk/Laterne.java
+++ smart-light/src/main/java/nk/Laterne.java
@@ -1,9 +1,12 @@
 package nk;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.opencsv.CSVReader;
 
 public class Laterne {
 	/*
@@ -47,6 +50,32 @@ public class Laterne {
 		double range = (max - min) + 1;     
 		return (double) (Math.random() * range) + min;
 	}
+	
+	
+	public static Laterne getKoordinaten(Laterne laterne) {
+
+		int size = 1;
+		try {
+			CSVReader reader = new CSVReader(new FileReader("cologne_data.csv"));
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+
+				if (size == (laterne.getLaternenID() * 100)) {
+
+					laterne.setX(Double.parseDouble(nextLine[3]));
+					laterne.setY(Double.parseDouble(nextLine[4]));
+
+					break;
+				}
+				size++;
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return laterne;
+	}
+	
 	
 	/*
 	 * getter, setter
